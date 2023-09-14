4502f6900d35411b3ef33025b9fecea619774c36
Furqan Ali <77490589+Furqanali111@users.noreply.github.com>
2023-09-14 16:07:59 +0500

    Add files via upload

---
+++ CodeSoft/Python Programing/Task 03/main.py
@@ -0,0 +1,22 @@
+import random
+import string
+def passwordgen(length):
+    data=string.ascii_letters+string.digits
+    password=''.join(random.choices(data,k=length))
+    return password
+
+
+
+    pass
+
+if __name__=="__main__":
+    try:
+        length=int(input("Enter the desired length of the password: "))
+        print("Your generated  password is :",passwordgen(length))
+    except ValueError:
+        print("Enter a integer")
+        try:
+            length = int(input("Enter the desired length of the password:  "))
+            print("your generated  password is :",passwordgen(length))
+        except ValueError:
+            print("Try Again")
